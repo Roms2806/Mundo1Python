6aa4e91845a9ebe9a7d0b03bed5efadb02e7888d
Roms <ramilamacedo6@gmail.com>
2023-03-28 13:31:49 -0300

    update sobre operadores Python

--- mundo1py/Primeiros passos/sum-decimal.py
+++ mundo1py/Primeiros passos/sum-decimal.py
@@ -11,3 +11,22 @@ print(x1 - x2) #subtração
 print(x1 * x2) #multiplicação
 print(x1 / x2) #divisão
 
+#valor booleano são avaliados False quando: valores forem vazios
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool({})
+bool([])
+
+class myclass():
+    def __len__(self):
+        return 0
+x = myclass()
+print(bool(x)) #retornará falso
+
+#x = 2j
+x = -200
+print(isinstance(x , int)) #isinstance é uma função interna outra maneira de retornar bool
+
