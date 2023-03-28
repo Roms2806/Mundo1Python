d34f52ec067686c1cc0820501d1110416329f041
Roms <ramilamacedo6@gmail.com>
2023-03-27 22:35:17 -0300

    Tipos de dados

---
+++ mundo1py/Primeiros passos/TiposDados.py
@@ -0,0 +1,43 @@
+x = "hello, world" (str)
+#definido:
+x = str("hello, world")
+x = 20 (int)
+#definido:
+x = int(20)
+x = 20.5 (float)
+#definido:
+x = float(20.5)
+x = 1j (complex)
+#definido:
+x = complex(1j)
+x = ["banana", "maçã", "pera"] (list)
+#definido:
+x = list(("banana", "maçã", "pera"))
+x = ("banana", "maçã", "pera") (tuple)
+#definido:
+x = tuple(("banana", "maçã", "pera"))
+x = range(6) (range)
+#definido:
+x = range(6)
+x = {"nome": "John", "age": 29} (dict)
+#definido:
+x = dict(name="John", age=29)
+x = {"banana", "maçã", "pera"} (set)
+#definido:
+x = set(("banana", "maçã", "pera"))
+x = frozenset({"banana","maçã", "pera"}) (frozenset)
+#definido:
+x = frozenset(("banana", "maçã", "pera"))
+x = True (bool)
+#definido:
+x = bool(5)
+x = b"hello" (bytes)
+#definido:
+x = bytes(5)
+x = bytearray(5) (bytearray)
+#definido:
+x = bytearray(5)
+x = memoryview(bytes(5)) (memoryview)
+#definido:
+x = memoryview(bytes(5))
+x = None (Nonetype)
