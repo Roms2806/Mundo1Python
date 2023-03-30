717d219b766d80d1b119320d50800039531029e1
Roms <ramilamacedo6@gmail.com>
2023-03-29 23:20:47 -0300

    update com métodos strings

--- mundo1py/Tratando dados e Fazendo Contas/antecessor-sucessor.py
+++ mundo1py/Tratando dados e Fazendo Contas/antecessor-sucessor.py
@@ -1,3 +1,5 @@
+#
+
 n = int(input('Digite um numero: '))
 antecessor = n - 1
 sucessor = n + 1
--- mundo1py/Tratando dados e Fazendo Contas/dissecando-variável.py
+++ mundo1py/Tratando dados e Fazendo Contas/dissecando-variável.py
@@ -1,3 +1,7 @@
+#Faça um programa que leia algo pelo teclado 
+#Mostre seu tipo primitivo 
+#E todas as informações possíveis
+
 x1 = (input('digite algo: '))
 
 print('O tipo primitivo é', type(x1))
@@ -12,3 +16,11 @@ print('Temos letras minúsculas?', x1.islower())
 print('Todos os caracteres são espaços em branco?', x1.isspace())
 print('Temos letras em caixa alta?', x1.isupper())
 
+#mais alguns métodos de strings
+capitalize() #converte o 1º caracter em maiúscula
+casefold() #converte strings em letras minúsculas
+center() #retorna uma string centralizada
+count() #retorna o nº de vezes que um valor especificado ocorre em uma string
+encode() #retorna uma versão codificada da string
+endswith() #retorna TRUE se a string terminar com valor espcificado
+expandtabs() #define o tamanho da tabulação da string
