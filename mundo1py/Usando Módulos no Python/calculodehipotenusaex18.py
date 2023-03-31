d5d453afe6e30542a64ae65c55eb6b91232e8620
Roms <ramilamacedo6@gmail.com>
2023-03-30 22:51:41 -0300

    update Biblioteca Math

--- mundo1py/Usando Módulos no Python/calculodehipotenusaex18.py
+++ mundo1py/Usando Módulos no Python/calculodehipotenusaex18.py
@@ -1,3 +1,5 @@
+# >Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. 
+# >Calcule e mostre o comprimento da hipotenusa
 
 '''co = int(input('Digite um valor para o cateto oposto:'))
 ca = int(input(' Digite um valor para o cateto adjacente:'))
@@ -7,4 +9,8 @@ print('Calculando a hipotenusa, a²+b²=c², seu comprimento vale {:.3f}'.format
 from math import hypot
 co = int(input(" Digite um valor para o cateto oposto:"))
 ca = int(input(' Digite um valor para o cateto adjacente:'))
-print('Calculando a hipotenusa seu valor é {}'.format(hypot(co, ca)))
+print('Calculando a hipotenusa seu valor é {:.2f}'.format(hypot(co, ca)))
+
+#math.hipot> retorna norma euclidiana
+#Norma Euclidiana: é a distância da origem às coordenadas dadas. É derivada do teorema de pitágoras
+#O teorema afirma que um triângulo retangulo, o quadrado  do comprimento da hipotenusa é igual à soma dos quadrados dos outros dois lados.
--- mundo1py/Usando Módulos no Python/porção inteira.py
+++ mundo1py/Usando Módulos no Python/porção inteira.py
@@ -9,3 +9,9 @@ print('A parte inteira do valor {} corresponde a {}'.format(num, n))'''
 from math import trunc
 num = float(input('Digite um valor: '))
 print('A porção inteira do valor {} é {}'.format(num, trunc(num)))
+
+#módulo math = módulo matemático 
+#import trunc > retorna as partes inteiras truncadas de um número e não arredonda o número, mas remove os decimais
+
+#tem que importar a biblioteca math para poder usar o módulo trunc 
+print(math.trunc(2.77))
